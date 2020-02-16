a6c1b9c3761ef0fcc4062e0a498efa2334feea64
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-02-16 23:37:23 +0300

    test(operations): test response generation logic

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -5,7 +5,79 @@ import app from './helpers/app';
 const request = supertest(app);
 
 describe('middleware', () => {
-  it('should work', async () => {
+  it("should return fields' example values if they are set", async () => {
+    const response = await request.get('/api/pet/2');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('name', 'doggie');
+    expect(response.body).toHaveProperty('photoUrls', ['http://test.test', 'http://test1.test1']);
+  });
+
+  it('should return the first example from the examples list in the response', async () => {
+    const response = await request.get('/api/pet/someOperation');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('name', 'first example name');
+    expect(response.body).toHaveProperty('photo', 'http://some-url-first.com');
+  });
+
+  it('should return randomly generated values if examples are not specified for them', async () => {
+    const response = await request.get('/api/pet/someOtherOperation');
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty('name');
+    expect(typeof response.body.name).toBe('string');
+
+    expect(response.body).toHaveProperty('photo');
+    expect(typeof response.body.photo).toBe('string');
+    expect(response.body.photo.startsWith('http')).toBe(true);
+  });
+
+  it('should return values of the given faker type if they are specified', async () => {
+    const response = await request.post('/api/pet/2');
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty('id');
+    expect(typeof response.body.id).toBe('number');
+
+    expect(response.body).toHaveProperty('name');
+    expect(typeof response.body.name).toBe('string');
+  });
+
+  it('should return an 404 error response if the given url does not exist', async () => {
+    const response = await request.get('/api/pet-not-exist/2');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('message', 'Not found');
+  });
+
+  it.skip('should return an 400 error response if path params are not valid', async () => {
+    const response = await request.get('/api/pet/2');
+
+    expect(response.status).toBe(200);
+  });
+
+  it.skip('should return an 400 error response if query params are not valid', async () => {
+    const response = await request.get('/api/pet/2');
+
+    expect(response.status).toBe(200);
+  });
+
+  it.skip('should return an 400 error response if headers params are not valid', async () => {
+    const response = await request.get('/api/pet/2');
+
+    expect(response.status).toBe(200);
+  });
+
+  it.skip('should return an 401 error response if security schema params are not valid', async () => {
+    const response = await request.get('/api/pet/2');
+
+    expect(response.status).toBe(200);
+  });
+
+  it.skip('should return an 400 error response on invalid content-type response', async () => {
     const response = await request.get('/api/pet/2');
 
     expect(response.status).toBe(200);
