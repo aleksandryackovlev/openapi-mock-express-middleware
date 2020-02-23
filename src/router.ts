756034a2efbdc58cbc63e4d01be71d5ab5006890
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-02-23 19:43:13 +0300

    chore(router): configure cors

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const createRouter = (): express.Router => {
   const router = express.Router();
 
+  router.use(cors());
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
   router.use(methodOverride());
